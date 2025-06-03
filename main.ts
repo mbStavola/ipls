d08128425d9a0754325f9a64ba4795ee52871003
Matt Stavola <mstavola9@gmail.com>
2025-06-03 15:10:29 -0400

    Add more header-derived ips

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 Deno.serve((req, info) => {
   const ip = info.remoteAddr.hostname;
   const cfConnectingIp = req.headers.get('cf-connecting-ip') ?? undefined;
+  const cfConnectingIpV6 = req.headers.get('cf-connecting-ipv6') ?? undefined;
+  const xRealIp = req.headers.get('x-real-ip') ?? undefined;
+  const trueClientIp = req.headers.get('true-client-ip') ?? undefined;
   const xForwardedFor = req.headers.get('x-forwarded-for')
     ?.split(',')
     ?.map((ip) => ip.trim())
@@ -9,6 +12,9 @@ Deno.serve((req, info) => {
   return new Response(JSON.stringify({
     ip,
     cfConnectingIp,
+    cfConnectingIpV6,
+    xRealIp,
+    trueClientIp,
     xForwardedFor,
   }), {
     headers: {
